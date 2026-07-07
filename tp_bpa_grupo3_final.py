345209255ea8b6954836e37677e5bf8f425be53e
Ricardo Mostacero <77706482+RickZ-V@users.noreply.github.com>
2026-07-06 23:03:29 -0500

    Update tp_bpa_grupo3_final.py

--- tp_bpa_grupo3_final.py
+++ tp_bpa_grupo3_final.py
@@ -45,7 +45,7 @@ tab_data, tab_train, tab_predict = st.tabs([
 # Carga y limpieza de datos estructurada
 @st.cache_data
 def cargar_y_procesar_datos():
-    df = pd.read_csv('Multas_transito_Pagadas_Ene_Mar_2026_I_TRIM_SAT.csv')
+    df = pd.read_csv('Multas_Pagadas.csv')
     
     if 'Año pago' in df.columns:
         df.drop(columns=['Año pago'], inplace=True)
